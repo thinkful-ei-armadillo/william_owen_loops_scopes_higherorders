91702a1b35d3f9e288601c23830fa3421a13b012
owencyr <cyroc@dukes.jmu.edu>
2019-01-16 07:35:09 -0500

    profile lookup solution updated/working

--- hO.js
+++ hO.js
@@ -7,7 +7,7 @@ function repeat(fn, n) {
 }
 const hello = () => console.log('Hello world');
 const goodbye = () => console.log('Goobye world');
-console.log(repeat(goodbye, 5));
+// console.log(repeat(goodbye, 5));
 
 function filter(arr, fn) {
     let newArray = [];
@@ -35,7 +35,7 @@ const filteredNames = filter(myNames, function(name) {
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 const filterName = filter(myNames, name => name[0] === 'R')
-console.log(filterName) // => ['Rich', 'Ray']
+// console.log(filterName) // => ['Rich', 'Ray']
 
 function hazardWarningCreator(typeOfWarning) {
     let warningCounter = 0;
@@ -51,25 +51,24 @@ const rocksWarning = hazardWarningCreator('Rocks on the Road');
 const mudslideWarning = hazardWarningCreator('MUDSLIDE!');
 const tornadoWarning = hazardWarningCreator('TORNADO!');
 
-rocksWarning('Centinela Ave and Olympic Blvd');
-mudslideWarning('Centinela Ave and Olympic Blvd');
-tornadoWarning('Centinela Ave and Olympic Blvd');
+// rocksWarning('Centinela Ave and Olympic Blvd');
+// mudslideWarning('Centinela Ave and Olympic Blvd');
+// tornadoWarning('Centinela Ave and Olympic Blvd');
 
-rocksWarning('Centinela Ave and Olympic Blvd');
-mudslideWarning('Centinela Ave and Olympic Blvd');
-tornadoWarning('Centinela Ave and Olympic Blvd');
+// rocksWarning('Centinela Ave and Olympic Blvd');
+// mudslideWarning('Centinela Ave and Olympic Blvd');
+// tornadoWarning('Centinela Ave and Olympic Blvd');
 
 
 const turtleMove = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
-const moves = turtleMove.filter((move) => (move[0] >= 0 && move[1] >= 0))
-   .map(item => item[0] + item[1])
-   .forEach(item => console.log(item));
+// const moves = turtleMove.filter((move) => (move[0] >= 0 && move[1] >= 0))
+//    .map(item => item[0] + item[1])
+//    .forEach(item => console.log(item));
 
 
 const input = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 const listInput = input.split(' ');
-console.log(listInput);
 const reducedInput = listInput.reduce((acc, word) => (word.length === 3 ? acc+= " " : acc+=word[word.length-1].toUpperCase()), " ");
 
 // const reducedInput = listInput.reduce(iterator, "")
@@ -85,4 +84,81 @@ function iterator(acc, word) {
 }
 
 
-console.log(reducedInput);
+// console.log(reducedInput);
+
+
+//Setup
+const contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop) {
+// MENTOR GUIDED SOLUTION: MAIN DIFFERENCE THAT IT ASKS EACH ENTRY IF IT IS THE NAME,
+// IF NOT, IT CHECKS NEXT ENTRY, NO OUTPUT UNTIL THE FOR LOOP HAS CHECKED THE ENTIRE CONTACT DATABASE
+    for(contact in contacts) {
+      if(contact.first == name){
+        if(contact.hasOwnProperty(prop)){
+          return contact[prop];
+        }
+        else {
+          return 'No Such Property';
+        }
+      }
+    }
+    return 'No Such Contact';
+  }
+//   TERNARY ATTEMPT AT SOLUTION, W/O UNDERSTANDING FOR..IN LOOP
+//  for (let element in contacts) {
+//    return name !== contacts[i].firstName) && contacts[i].prop === undefined) ? element.[prop]
+//        : name !== .firstName ? 'No Such Contact'
+//        : 'No Such Property';
+//  }
+// 
+
+
+// IF LOOP ATTEMPT THAT RETURNS ERROR ON EVERY ITERATION, RATHER THAN ONLY AFTER CHECKING ALL VALUES
+// for (let i in contacts) {
+//     console.log(contacts[i], i); //report current contacts index, as well as index value
+//     if (name !== contacts[i].firstName) {
+//         console.log(name, contacts[i].firstName);
+//         return 'No Such Contact'
+//     }
+//     else if(contacts[i].prop === undefined) {
+//         console.log(prop, contacts[i].[prop])
+//         return 'No Such Property'
+//     }
+//     else {
+//         console.log(contacts[i].[prop])
+//         return contacts[i].[prop]
+//     }
+//   }
+// }
+
+// Only change code above this line
+// Change these values to test your function
+console.log(lookUpProfile("Akira", "likes"));
+// console.log(lookUpProfile("Bob", "likes"));
